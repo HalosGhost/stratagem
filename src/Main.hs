064f0ee330b8b4f27492af7653b5265bea9ce3de
HalosGhost <halosghost@archlinux.info>
2015-06-29 18:18:58 -0500

    Setup the beginnings of a main menu

--- src/Main.hs
+++ src/Main.hs
@@ -2,24 +2,43 @@
 
 module Main where
 
-import Graphics.Vty hiding (pad)
-import Graphics.Vty.Widgets.All
-import System.Exit
+import           Graphics.Vty             hiding (pad)
+import           Graphics.Vty.Widgets.All
+import           System.Exit
+import qualified Data.Text                as     T
 
 fg = white
 bg = black
 
 main :: IO ()
-main = do pt <- plainText "Hello, World!" >>= withNormalAttribute (fgColor fg)
-          ui <- return pt
+main = do lst <- newList 1
+          setSelectedUnfocusedAttr lst $ Just (fgColor brightGreen)
+          sel <- vLimit 5 lst
+
+          let i1 = "Local PvP"
+              i2 = "Local PvC"
+              i3 = "Online PvP"
+
+          listComment <- plainText T.empty
+
+          addToList lst i1 =<< plainText i1
+          addToList lst i2 =<< plainText i2
+          addToList lst i3 =<< plainText i3
+
+          lst `onSelectionChange` \ev ->
+              case ev of
+                   SelectionOn _ k _ -> setText listComment k
+                   SelectionOff      -> return ()
 
           fg <- newFocusGroup
-          fg `onKeyPressed` \_ k _ -> do
+          fg `onKeyPressed` \_ k _ ->
              case k of
-                  KEsc -> exitSuccess
-                  _    -> return False
+                  KChar 'Q' -> exitSuccess
+                  _         -> return False
 
-          addToFocusGroup fg ui
+          addToFocusGroup fg sel
+          addToFocusGroup fg listComment
           c <- newCollection
-          addToCollection c ui fg
-          runUi c $ defaultContext { focusAttr = white `on` black }
+          addToCollection c sel fg
+          addToCollection c listComment fg
+          runUi c $ defaultContext { normalAttr = white `on` black }
